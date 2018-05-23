e8bb092c1c4db5f6f880687e538201a5c4b22247
Brandon Leichty <brandonl@me.com>
2018-05-23 16:32:03 -0500

    added validator

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
 // Create your Mongoose schema and models. Your database schema should match the following requirements:
@@ -10,25 +12,21 @@ mongoose.Promise = global.Promise;
 //  rating (Number, required, must fall between “1” and “5”)
 //  review (String)
 
-
 const reviewSchema = new Schema({
-  user: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  postedOn: {
-      type: Date,
-      default: Date.now,
-
-  },
-  rating: {
-      type: Number,
-      min: [1, 'Rating too low. Must be between 1 - 5.'],
-      max: [5, 'Rating too high. Must be between 1 - 5.'],
-      required: 'Must supply a review.',
-
-  },
-  review: {
-    type: String
-  }
+	user: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	postedOn: {
+		type: Date,
+		default: Date.now
+	},
+	rating: {
+		type: Number,
+		min: [1, 'Rating too low. Must be between 1 - 5.'],
+		max: [5, 'Rating too high. Must be between 1 - 5.'],
+		required: 'Must supply a review.'
+	},
+	review: {
+		type: String
+	}
 });
-
 
 module.exports = mongoose.model('Review', reviewSchema);
