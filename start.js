debdcb1ed74b599009f7e53843d25f3db4162b20
Brandon Leichty <brandonl@me.com>
2018-05-23 16:29:34 -0500

    dada is now seeded using mais-mongoose-seeder

--- start.js
+++ start.js
@@ -1,36 +1,46 @@
 const mongoose = require('mongoose');
-const seeder = require('mongoose-seeder');
-const seedData = require('./data/data.json');
+const seeder = require('mais-mongoose-seeder')(mongoose);
+const data = require('./src/data/data.json');
+// const seeder = require('mongoose-seeder');
+
+// Import all of our models
+require('./src/models/Course');
+require('./src/models/User');
+require('./src/models/Review');
 
 // Import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
 // Set default database
-const mongoDB = 'mongodb://localhost/CourseRatingAPI';
+const mongoDB = process.env.DATABASE || 'mongodb://localhost/CourseRatingAPI';
 
 // Tell Mongoose to use ES6 promises
 mongoose.Promise = global.Promise;
 
 // Connect to default Mongo database
-mongoose.connect(mongoDB).then(
-	() => {
+mongoose
+	.connect(mongoDB)
+	.then(() => {
 		console.log('👍👍 Successfully connected to Mongo database! 👍👍');
-	},
-	err => {
-		console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
-	}
-);
-
-// See database using mongoose-seeder. "dropDatabase" is set to true by default.
-const db = mongoose.connection;
-
-db.once('open', () => {
-	seeder.seed(seedData).then(() => {
-    console.log('Successfully seeded sample data! 🌻');
-  }).catch((err) => {
-		console.log(err);
+		seeder
+			.seed(data, {
+				dropDatabase: true,
+				dropCollections: false
+			})
+			.then(() => {
+				// The database objects are stored in dbData
+				console.log('Sample data successfully seeded! 🌻');
+			})
+			.catch(function(err) {
+				// handle error
+			});
+	})
+	.catch(function(err) {
+		// handle error
 	});
-});
+
+// See database using mongoose-seeder. dropDatabase is set to true by default.
+const db = mongoose.connection;
 
 // Start our app!
 const app = require('./src/app');
