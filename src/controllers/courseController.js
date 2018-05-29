df0929659f5c08c0adc64a87cd74a86f9afb6e69
Brandon Leichty <brandonl@me.com>
2018-05-29 17:37:04 -0500

    gets a specific course and adds a new course (newCourse isn't 100%)

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Course = mongoose.model('Course');
 const Review = mongoose.model('Review');
 
-
 // GET /api/courses 200 - Returns the Course "_id" and "title" properties
 exports.getAllCoures = (req, res, next) => {
 	Course.find({})
@@ -14,10 +13,21 @@ exports.getAllCoures = (req, res, next) => {
 		});
 };
 
-
 //  GET /api/course/:courseId 200 - Returns all Course properties and related documents for the provided course ID
 exports.getCourse = (req, res, next) => {
-	Course.findById({_id: req.params.courseId})
+	Course.findById({ _id: req.params.courseId })
+		.populate({
+			path: 'user',
+			model: 'User'
+		})
+		.populate({
+			path: 'reviews',
+			model: 'Review',
+			populate: {
+				path: 'user',
+				model: 'User'
+			}
+		})
 		.exec((err, course) => {
 			if (err) return next(err);
 			res.status(200);
@@ -25,10 +35,11 @@ exports.getCourse = (req, res, next) => {
 		});
 };
 
-
 //  POST /api/courses 201 - Creates a course, sets the Location header, and returns no content
 exports.newCourse = (req, res, next) => {
-
+  Course.create(req.body), (err, user)=> {
+    if (err) return next(err);
+	};
+	console.log('CREATED A NEW COURSE! 💩');
+  res.status(201).location('/').end();
 };
-
-
