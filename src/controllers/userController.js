54a40783821a07c9b9a3039d46ffcbd824ccc8be
Brandon Leichty <brandonl@me.com>
2018-05-30 16:34:59 -0500

    refactored newuser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,12 @@ const auth = require('basic-auth');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
+
+// Update any POST and PUT routes to return Mongoose validation errors.
+//  Use the next function in each route to pass any Mongoose validation errors to Express’s global error handler
+//  Send the Mongoose validation error with a 400 status code to the user
+
+
 //  GET /api/users 200 - Returns the currently authenticated user
 exports.getCurrentUser = (req, res, next) => {
 	User.findOne({ emailAddress: req.credentials.name })
@@ -15,13 +21,16 @@ exports.getCurrentUser = (req, res, next) => {
 
 //  POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
 exports.newUser = (req, res, next) => {
-	User.create(req.body),
-		(err, user) => {
-			console.log('Heyyyy this works!!');
-			if (err) return next(err);
-		};
-	res
-		.status(201)
-		.location('/')
-		.end();
+	User.create(req.body, (err, user) => {
+		if (err) {
+			console.log(`Oops! There was an error: ${err.message}`);
+			return next(err);
+		} else {
+			console.log(`Successfully created a new user profile for ${req.body.fullName}.`);
+			res
+				.status(201)
+				.location('/')
+				.end();
+		}
+	});
 };
