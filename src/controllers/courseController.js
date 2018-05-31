acc6488ece2cd45f627e75b5a46761454d54a95b
Brandon Leichty <brandonl@me.com>
2018-05-30 21:44:47 -0500

    newCourseReview is now functional

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -13,8 +13,6 @@ exports.getAllCoures = (req, res, next) => {
 		});
 };
 
-
-
 //  GET /api/course/:courseId 200 - Returns all Course properties and related documents for the provided course ID
 exports.getCourse = (req, res, next) => {
 	Course.findById({ _id: req.params.courseId })
@@ -37,8 +35,6 @@ exports.getCourse = (req, res, next) => {
 		});
 };
 
-
-
 //  POST /api/courses 201 - Creates a course, sets the Location header, and returns no content
 exports.newCourse = (req, res, next) => {
 	Course.create(req.body, (err, course) => {
@@ -55,23 +51,58 @@ exports.newCourse = (req, res, next) => {
 	});
 };
 
-
-
 //  PUT /api/courses/:courseId 204 - Updates a course and returns no content
 exports.updateCourse = (req, res, next) => {
 	// if the updated course has an _id field, remove it before making the updates. A Mongo _id is immutable - so you'll get an error if you try to udpate it
-	if(req.body._id) {
+	if (req.body._id) {
 		console.log(req.body._id);
 		delete req.body._id;
 	}
 	// Update the course and throw an error if one occurs
-	Course.update({ _id: req.params.courseId }, req.body, (err) => {
-			if (err) {
-				console.log(`Oops! There was an error: ${err.message}`);
-				return next(err);
-			} else {
-				res.status(204).end();
-			}
+	Course.update({ _id: req.params.courseId }, req.body, err => {
+		if (err) {
+			console.log(`Oops! There was an error: ${err.message}`);
+			return next(err);
+		} else {
+			res.status(204).end();
 		}
-	);
+	});
+};
+
+//  POST /api/courses/:courseId/reviews 201 - Creates a review for the specified course ID, sets the Location header to the related course, and returns no content
+exports.newCourseReview = (req, res, next) => {
+
+	// create a new review and save it to the db.
+	new Review(req.body).save((err, review) => {
+		if (err) {
+			console.log(`Oops! There was an error: ${err.message}`);
+			return next(err); // pass error to global handler
+		} else {
+
+			// if no errors add the review to the appropriate course
+			Course.findByIdAndUpdate(
+				{ _id: req.params.courseId },
+				{
+					$push: {
+						review
+					}
+				},
+				{
+					upsert: true,
+					new: true
+				}
+			).exec((err, updatedCourse) => {
+				if (err) {
+					console.log(`Oops! There was an error: ${err.message}`);
+					return next(err);
+				} else {
+					console.log(`Successfully added a new review to the course.`);
+					res
+						.status(201)
+						.location('/')
+						.end();
+				}
+			});
+		}
+	});
 };
