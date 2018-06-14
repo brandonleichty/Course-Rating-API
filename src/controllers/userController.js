f66a821ac37c8b1f31d76126717e580b75efebd0
Brandon Leichty <brandonl@me.com>
2018-06-13 22:03:15 -0500

    return full user document instead of just name

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,18 +4,17 @@ const User = mongoose.model('User');
 
 
 // Update any POST and PUT routes to return Mongoose validation errors.
-//  Use the next function in each route to pass any Mongoose validation errors to Express’s global error handler
-//  Send the Mongoose validation error with a 400 status code to the user
+// Use the next function in each route to pass any Mongoose validation errors to Express’s global error handler
+// Send the Mongoose validation error with a 400 status code to the user
 
 
 //  GET /api/users 200 - Returns the currently authenticated user
 exports.getCurrentUser = (req, res, next) => {
 	User.findOne({ emailAddress: req.credentials.name })
-		.select('fullName')
 		.exec((err, user) => {
 			if (err) return next(err);
 			console.log(user.fullName);
-			res.status(200).json({ data: user.fullName });
+			res.status(200).json({ data: user});
 		});
 };
 
